33b29cd6e93c6698201ae35c96e36748efedc7c8
Remsko <pinoit.remi@gmail.com>
2018-10-31 22:05:57 +0100

    maj headder

--- incs/memory.h
+++ incs/memory.h
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/29 20:37:01 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/10/29 20:56:25 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/10/31 22:05:37 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include <stdlib.h>
 
-# define MEM_WORD_LEN sizeof(unsigned long int)
+# define MEM_WORD_LEN (sizeof(unsigned long int))
 # define MEM_BLOCK_SIZE (8 * MEM_WORD_LEN)
+# define MEM_PAGE_SIZE	(4096)
 
 #endif
