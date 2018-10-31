4ff31f50f0767ba922127636250b472934d548e9
Remsko <pinoit.remi@gmail.com>
2018-10-31 22:03:33 +0100

    ft_memcpy.c begin

---
+++ srcs/memory/ft_memcpy.c
@@ -0,0 +1,35 @@
+
+
+void byte_copy_forward(unsigned long int dst_bp, unsigned long int src_bp, size_t nbytes)
+{
+    while (nbytes > 0)
+    {
+        ((unsigned char *)dst_bp)[0] = ((unsigned char *)src_bp)[0];
+        src_bp += 1;
+        dst_bp += 1;
+        nbytes -= 1;
+    }
+}
+
+void
+
+void *ft_memcpy(void *dstpp, const void *srcpp, size_t len)
+{
+    unsigned long int dstp;
+    unsigned long int srcp;
+
+
+    dstp = (long int)dstpp;
+    srcp = (long int)srcpp;
+    if (len >= MEM_WORD_LEN)
+    {
+        len -= (-dstp) % MEM_WORD_LEN;
+        byte_copy_forward(dstp, srcp, (-dstp) % MEM_WORD_LEN);
+        /*
+        PAGE_COPY_FWD_MAYBE(dstp, srcp, len, len);
+        WORD_COPY_FWD(dstp, srcp, len, len);
+        */
+    }
+    byte_copy_forward(dstp, srcp, len);
+    return (dstpp);
+}
