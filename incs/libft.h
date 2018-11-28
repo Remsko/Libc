7bdf684d0d61babc372835543ef38ba0c976b2d8
Remi PINOIT <rpinoit@e6r2p3.vp.42.fr>
2018-11-28 16:40:10 +0100

    feat: all header in libft header

--- incs/libft.h
+++ incs/libft.h
@@ -6,14 +6,18 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 20:35:30 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/11/25 16:35:41 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/11/28 16:39:46 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-#include "string_42.h"
-#include "memory_42.h"
+# include "array_42.h"
+# include "ascii_42.h"
+# include "libft.h"
+# include "math_42.h"
+# include "memory_42.h"
+# include "string_42.h"
 
 #endif
