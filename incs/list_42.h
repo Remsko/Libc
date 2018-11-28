24caeaf2b58c65616894bef7be7ba04c67908360
Remi PINOIT <rpinoit@e6r2p3.vp.42.fr>
2018-11-28 17:42:52 +0100

    feat: list header

--- incs/list_42.h
+++ incs/list_42.h
@@ -1,24 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   list_42.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/10/25 20:35:30 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/11/28 17:42:31 by rpinoit          ###   ########.fr       */
+/*   Created: 2018/11/28 17:38:11 by rpinoit           #+#    #+#             */
+/*   Updated: 2018/11/28 17:42:19 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#ifndef LIST_42_H
+# define LIST_42_H
 
-# include "array_42.h"
-# include "ascii_42.h"
-# include "libft.h"
-# include "math_42.h"
-# include "memory_42.h"
-# include "string_42.h"
-# include "list_42.h"
+typedef struct      s_list
+{
+    struct s_list   *prev;
+    struct s_list   *next;
+}                   t_list;
 
 #endif
