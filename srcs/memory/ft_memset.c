d77a0f8da8d93b0039c79e5ef33c58adfa35a0d1
Remsko <pinoit.remi@gmail.com>
2019-04-13 23:13:47 +0200

    remove insane while in memset

--- srcs/memory/ft_memset.c
+++ srcs/memory/ft_memset.c
@@ -12,13 +12,13 @@
 
 #include "memory_42.h"
 
-inline static void	align_word(unsigned char **pdst, unsigned char c, size_t *len)
+inline static void	align_word(unsigned char **pdst, unsigned char c, size_t offset)
 {
-	while ((size_t)(*pdst) % MEM_WORD_LEN != 0)
+	while (offset > 0)
 	{
 		(*pdst)[0] = c;
 		*pdst += 1;
-		*len -= 1;
+		offset -= 1;
 	}
 }
 
@@ -59,7 +59,7 @@ inline static void	set_bytes(unsigned char **pdst, unsigned char c, size_t bytes
 	}
 }
 
-void				*ft_memset(void *dst, int c, size_t len)
+void			*ft_memset(void *dst, int c, size_t len)
 {
 	unsigned long	cccc;
 
@@ -69,7 +69,8 @@ void				*ft_memset(void *dst, int c, size_t len)
 		cccc |= cccc << 8;
 		cccc |= cccc << 16;
 		cccc |= cccc << 32;
-		align_word((unsigned char **)&dst, (unsigned char)c, &len);
+		align_word((unsigned char **)&dst, (unsigned char)c, (unsigned long)dst % MEM_WORD_LEN);
+		len -= (unsigned long)dst % MEM_WORD_LEN;
 		set_blocks((unsigned long **)&dst, cccc, len / MEM_BLOCK_SIZE);
 		len %= MEM_BLOCK_SIZE;
 		set_words((unsigned long **)&dst, cccc, len / MEM_WORD_LEN);
