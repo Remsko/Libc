3e30ebb4185d1d1911bda4bc00c74412c0898538
Remsko <pinoit.remi@gmail.com>
2019-04-13 23:10:08 +0200

    recup strwords

--- srcs/string/ft_strwords.c
+++ srcs/string/ft_strwords.c
@@ -11,18 +11,26 @@
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include <stdbool.h>
 
 size_t	ft_strwords(const char *s, char c)
 {
 	size_t	words;
+	bool	flag;
 
+	flag = 0;
 	words = 0;
 	if (s != NULL)
 	{
 		while (*s != '\0')
 		{
-			if (*s != c && (*s + 1 == c || *s + 1 == '\0'))
+			if (*s == c)
+				flag = 0;
+			else if (flag == 0)
+			{
+				flag = 1;
 				++words;
+			}
 			++s;
 		}
 	}
